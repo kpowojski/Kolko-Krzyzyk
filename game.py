6b22b9dd6a5c36134209ce7a136962702ea80c0d
kpowojski <krystian.powojski@hotmail.com>
2016-10-08 10:44:15 +0200

    Ability to play game

--- game.py
+++ game.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 PLAYER_X = []
 PLAYER_O = []
 
+#table which stores all marks placed by players
+#TAB = ['x', 'x',  'x', 'o', ' ', ' ', ' ', ' ', ' ']
+TAB = [' '] *9
 turn = None
 
 def print_game_init_screen():
@@ -25,42 +29,46 @@ def x_sign(row_number):
 		return ' ' + letter + ' ' +letter
 	elif row_number == 4:
 		return letter +'   ' + letter
+	else:
+		return ' ' * 5
 
 def o_sign(row_number):
 	if row_number == 0: 
 		return ' ' + '_'*3 + ' '
-	elif row_numerb == 1:
+	elif row_number == 1:
 		return '|   |'
-	elif row_numerb == 2:
+	elif row_number == 2:
 		return '|   |'
-	elif row_numerb == 3:
+	elif row_number == 3:
 		return '|   |'
 	elif row_number == 4:
 		return '|___|'
-	
+	else:
+		return ' ' * 5
 	
 
 def game_table():
-	tab = ['x', ' ',  ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-
+	
 	for k in range(3):
 		print(' '*49)
 		for i in range(5):
+			sign_0 = ' ' * 5
 			sign_1 = ' ' * 5
 			sign_2 = ' ' * 5
-			sign_3 = ' ' * 5
 			
-			if tab[k] is 'x':
+			if TAB[k*3] == 'x':
 				sign_0 = x_sign(i)
-			elif tab[k+1] is 'x':
-				sign_1 = x_sign(i)
-			elif tab[k+2] is 'x':
-				sign_2 = x_sign(i)
-			elif tab[k] is 'o':
+			elif TAB[k*3] == 'o':
 				sign_0 = o_sign(i)
-			elif tab[k+1] is 'o':
+				
+			if TAB[(k*3)+1] == 'x':
+				sign_1 = x_sign(i)
+			elif TAB[(k*3)+1] == 'o':
 				sign_1 = o_sign(i)
-			elif tab[k+2] is 'o':
+			
+			if TAB[(k*3)+2] == 'x':
+				sign_2 = x_sign(i)
+			elif TAB[(k*3)+2] == 'o':
 				sign_2 = o_sign(i)
 			print('\t' + sign_0 + '\t|\t' + sign_1 + '\t|\t' + sign_2 + '\t') #normalna
 		if (k <2 ):
@@ -77,27 +85,39 @@ def validate(sign):
 	else:
 		return False
 
+def change_turn():
+	global turn
+	
+	if turn is 'X':
+		turn = 'O'
+	else:
+		turn = 'X'
+		
+		
+def assign_mark(sign):
+	global TAB
+	print('Kolejka: {}').format(turn)
+	position = int(sign, 10)
+	if TAB[position] is ' ':
+		TAB[position] = turn.lower()
+		print('Znak {} zostal dodany do planszy na pozycji {}').format(turn, position)
+		change_turn()
+	else:
+		print('Pozycja zajeta, sprobuj ponownie')
+		
 if __name__ == '__main__':
 	print_game_init_screen()
 	game_table()
 
-#	while(True):
-#		sign = input('Podaj pozycje na jakiej chcesz wstawic znak: ')
-#		if validate(sign):
-#			print('prawidlowy znak')
-#			
-#		else:
-#			print('nieprawidlowy znak')
-		
-
+	turn = 'X'
+	print('Gre rozpoczyna gracz {}').format(turn)
 	
-#Liter ma szerokość====5 i wysokość====5 	
-
-#\t5\t|\t5\t|\t5\t  
-#tabulator = 7 
-	#6*7 + 2 + 3*5 = 
-	#|   |
-	#|   |
-	#|   |
-	#|___|
-	       
+	while(True):
+		print('Kolej gracza {}').format(turn)
+		sign = raw_input('Podaj pozycje na jakiej chcesz wstawic znak: ')
+		if validate(sign):
+			assign_mark(sign)
+			
+		else:
+			print('Popelniles blad!\r\n\r\nProsze podac znak jeszcze raz.')
+		game_table()
