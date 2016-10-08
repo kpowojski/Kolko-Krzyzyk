0d7b3ffc44a0ccfa9ac48f24e74638b542761884
kpowojski <krystian.powojski@hotmail.com>
2016-10-08 11:56:58 +0200

    Game finished

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import collections
+
 
 PLAYER_X = []
 PLAYER_O = []
@@ -77,10 +79,11 @@ def game_table():
 	
 	
 def validate(sign):
+	print(len(sign))
 	if len(sign) != 1:
 		return False
-	if ord(sign) < 58:
-		if ord(sign) > 48:
+	if ord(sign) < 57:
+		if ord(sign) > 47:
 			return True
 	else:
 		return False
@@ -101,9 +104,37 @@ def assign_mark(sign):
 	if TAB[position] is ' ':
 		TAB[position] = turn.lower()
 		print('Znak {} zostal dodany do planszy na pozycji {}').format(turn, position)
-		change_turn()
 	else:
 		print('Pozycja zajeta, sprobuj ponownie')
+
+
+def verify_result():
+	winner = None
+	player = turn.lower()
+	
+	if TAB.count(player) > 2: # postawiono co najmniej znaki danego rodzaju na planszy
+		for i in range(len(TAB)):
+			if i == 0 or i == 3 or i == 6:
+				if TAB[i] == player and TAB[i+1] == player and TAB[i+2] == player:
+					winner = player
+			
+			if i == 0 or i == 1 or i ==2:
+				if TAB[i] == player and TAB[i+3] == player and TAB[i+6] == player:
+					winner = player
+			
+			if i ==0:
+				if TAB[i] == player and TAB[i+4] == player and TAB[i+8] == player:
+					winner = player
+			
+			if i ==2:
+				if TAB[i] == player and TAB[i+2] == player and TAB[i+4] == player:
+					winner = player
+	
+	if TAB.count(' ') == 0 and winner == None:
+		return 'END_of_GAME'
+	else:
+		return winner
+		
 		
 if __name__ == '__main__':
 	print_game_init_screen()
@@ -117,7 +148,16 @@ if __name__ == '__main__':
 		sign = raw_input('Podaj pozycje na jakiej chcesz wstawic znak: ')
 		if validate(sign):
 			assign_mark(sign)
-			
+			res = verify_result()
+			if res == 'END_of_GAME':
+				print('Koniec gry, brak zwyciezcy')
+				break
+			elif res != None:
+				print('Zwyciezca jest: {}').format(res)
+				game_table()
+				break
+			else:
+				change_turn()
 		else:
 			print('Popelniles blad!\r\n\r\nProsze podac znak jeszcze raz.')
 		game_table()
